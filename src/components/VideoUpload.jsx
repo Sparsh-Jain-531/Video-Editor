0053187947f07b4ae0ccccecbad0c41985808337
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-03-22 14:24:29 +0530

    Refactor VideoUpload component for improved readability and maintainability

--- src/components/VideoUpload.jsx
+++ src/components/VideoUpload.jsx
@@ -21,7 +21,7 @@ const VideoUpload = ({ onVideoUpload }) => {
 
   const VIDEO_WIDTH = 800;
   const VIDEO_HEIGHT = 430;
-  
+
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -39,8 +39,14 @@ const VideoUpload = ({ onVideoUpload }) => {
   };
 
   const setRatio = (widthRatio, heightRatio) => {
-    const newWidth = Math.min(VIDEO_WIDTH, (VIDEO_HEIGHT * widthRatio) / heightRatio);
-    const newHeight = Math.min(VIDEO_HEIGHT, (VIDEO_WIDTH * heightRatio) / widthRatio);
+    const newWidth = Math.min(
+      VIDEO_WIDTH,
+      (VIDEO_HEIGHT * widthRatio) / heightRatio
+    );
+    const newHeight = Math.min(
+      VIDEO_HEIGHT,
+      (VIDEO_WIDTH * heightRatio) / widthRatio
+    );
 
     setCrop({
       width: Math.floor(newWidth),
@@ -62,8 +68,16 @@ const VideoUpload = ({ onVideoUpload }) => {
       {!videoFile && (
         <div className="flex flex-col justify-center items-center h-72">
           <div className="text-3xl font-bold">Upload Video</div>
-          <div className="text-lg font-semibold">Easily crop video files online</div>
-          <input type="file" accept="video/*" onChange={handleFileChange} id="videoInput" hidden />
+          <div className="text-lg font-semibold">
+            Easily crop video files online
+          </div>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={handleFileChange}
+            id="videoInput"
+            hidden
+          />
           <button
             className="mt-4 bg-blue-500 py-2 px-4 text-white rounded-sm"
             onClick={() => document.getElementById("videoInput").click()}
@@ -75,11 +89,12 @@ const VideoUpload = ({ onVideoUpload }) => {
 
       {videoFile && (
         <div className="flex flex-col gap-4 justify-center items-center py-10 bg-blue-900 relative">
-          <div className="text-white text-sm">
-            {videoName}
-          </div>
+          <div className="text-white text-sm">{videoName}</div>
           {/* Video Player with 16:9 aspect ratio */}
-          <div className="relative" style={{ width: VIDEO_WIDTH, height: VIDEO_HEIGHT }}>
+          <div
+            className="relative"
+            style={{ width: VIDEO_WIDTH, height: VIDEO_HEIGHT }}
+          >
             <ReactPlayer
               ref={playerRef}
               url={videoFile}
@@ -116,52 +131,83 @@ const VideoUpload = ({ onVideoUpload }) => {
           {/* Video Controls & Cropping Options */}
           <div className="flex flex-col lg:flex-row items-center justify-center mt-4">
             <div className="flex justify-center items-center gap-4 lg:gap-2">
-            <button onClick={togglePlayPause} className=" p-2 bg-blue-950 text-white rounded-full">
-              {isPlaying ? <Pause fontSize="large" /> : <PlayArrow fontSize="large" />}
-            </button>
-            <button onClick={() => setRatio(VIDEO_WIDTH, VIDEO_HEIGHT)} className="mx-2 py-2 px-4 bg-blue-950 text-white rounded-lg">
-              Original
-            </button>
-            <button onClick={() => setRatio(16, 9)} className="py-2 px-4 bg-blue-950 text-white rounded-lg">
-              16:9
-            </button>
-            <button onClick={() => setRatio(9, 16)} className="py-2 px-4 bg-blue-950 text-white rounded-lg">
-              9:16
-            </button>
-            <span className="text-white min-w-28">
-              {crop.width}px x {crop.height}px
-            </span>
-            </div>
-            <div className="flex justify-center items-center mx-4 min-w-72">
-            <Slider
-              value={timeRange}
-              onChange={(e, newValue) => setTimeRange(newValue)}
-              min={0}
-              max={Math.floor(duration)}
-              valueLabelDisplay="auto"
-              className="flex-1 w-full"
-            />
-            </div>
-            <div className="flex justify-center items-center gap-4 lg:gap-2">
-            <button onClick={addTimeSection} className="p-2 bg-green-600 hover:bg-green-700 transition-all text-white rounded-lg min-w-28">
-              Add Section
-            </button>
-            <div className="flex justify-center items-center text-white min-w-28">
-              <span className="p-1 rounded">
-                X: {crop.x}px, Y: {crop.y}px
+              <button
+                onClick={togglePlayPause}
+                className=" p-2 bg-blue-950 text-white rounded-full"
+              >
+                {isPlaying ? (
+                  <Pause fontSize="large" />
+                ) : (
+                  <PlayArrow fontSize="large" />
+                )}
+              </button>
+              <button
+                onClick={() => setRatio(VIDEO_WIDTH, VIDEO_HEIGHT)}
+                className="mx-2 py-2 px-4 bg-blue-950 text-white rounded-lg"
+              >
+                Original
+              </button>
+              <button
+                onClick={() => setRatio(16, 9)}
+                className="py-2 px-4 bg-blue-950 text-white rounded-lg"
+              >
+                16:9
+              </button>
+              <button
+                onClick={() => setRatio(9, 16)}
+                className="py-2 px-4 bg-blue-950 text-white rounded-lg"
+              >
+                9:16
+              </button>
+              <span className="text-white min-w-28">
+                {crop.width}px x {crop.height}px
               </span>
             </div>
+            <div className="flex justify-center items-center mx-4 min-w-72">
+              <Slider
+                value={timeRange}
+                onChange={(e, newValue) => setTimeRange(newValue)}
+                min={0}
+                max={Math.floor(duration)}
+                valueLabelDisplay="auto"
+                className="flex-1 w-full"
+              />
+            </div>
+            <div className="flex justify-center items-center gap-4 lg:gap-2">
+              <button
+                onClick={addTimeSection}
+                className="p-2 bg-green-600 hover:bg-green-700 transition-all text-white rounded-lg min-w-28"
+              >
+                Add Section
+              </button>
+              <div className="flex justify-center items-center text-white min-w-28">
+                <span className="p-1 rounded">
+                  X: {crop.x}px, Y: {crop.y}px
+                </span>
+              </div>
             </div>
           </div>
 
           {/* Selected Time Sections */}
           <div>
-            {selectedSections.length > 0 && <div className="text-white text-lg font-semibold text-center mb-2">Selected Timelines - </div>}
+            {selectedSections.length > 0 && (
+              <div className="text-white text-lg font-semibold text-center mb-2">
+                Selected Timelines -{" "}
+              </div>
+            )}
             {selectedSections.map((section, index) => (
               <div key={index} className="text-white">
                 <div className="my-1 flex justify-start items-center gap-5">
-                  <div className="w-56"><span className="font-semibold font-serif">Time: </span>{`Start: ${section.start}s, End: ${section.end}s`}</div>
-                  <div><span className="font-semibold font-serif">Dimensions: </span>{`X: ${section.X}px, Y: ${section.Y}px`}</div>
+                  <div className="w-56">
+                    <span className="font-semibold font-serif">Time: </span>
+                    {`Start: ${section.start}s, End: ${section.end}s`}
+                  </div>
+                  <div>
+                    <span className="font-semibold font-serif">
+                      Dimensions:{" "}
+                    </span>
+                    {`X: ${section.X}px, Y: ${section.Y}px`}
+                  </div>
                 </div>
               </div>
             ))}
